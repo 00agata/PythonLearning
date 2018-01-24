6f1fe0aa62b91ac6a9276f45e4ad9ef0d183695c
Agata Wiewiora <agata.wiewiora@globallogic.com>
2018-01-24 23:02:37 +0100

    zad1 has been corrected according to review comments

--- Chapter1/Zad1.py
+++ Chapter1/Zad1.py
@@ -2,12 +2,16 @@
 Created on 23 sty 2018
 
 @author: agata.wiewiora
+
+This program reads two numbers: length of the base and the height of a right-angled triangle
+ and prints the area of this triangle
+ 
 '''
-# This program reads two numbers and length of the base and the height of a right-angled triangle and prints the area:
-print 'Podaj wielkosc podstawy trojkata'
+
+print ('Podaj wielkosc podstawy trojkata')
 a = int(input())
-print 'Podaj wysokosc trojkata'
+print ('Podaj wysokosc trojkata')
 h = int(input())
-pole = (a*h)/2
-print 'Pole trojkata o podstawie %i i wysokosci %i jest rowne %i' %(a,h,pole)
-print pole
+pole = (a * h) / 2
+print ('Pole trojkata o podstawie %i i wysokosci %i jest rowne %i' % (a, h, pole))
+print (pole)
