72fa5d664ee5e681631f3f87e49c0d682079c25b
Agata Wiewiora <agata.wiewiora@globallogic.com>
2018-01-25 01:00:04 +0100

    zad2 corrected

--- Chapter1/Zad2.py
+++ Chapter1/Zad2.py
@@ -3,11 +3,14 @@ Created on 24 sty 2018
 
 @author: agata.wiewiora
 
-This program will print the greeting for the user, who needs to provide his name
+This program will print the greeting for the user, 
+who needs to provide his name
 '''
+
+
 # Read  name of the user:
 user_name = raw_input("Please, write your name: ")
+
 # Print a greeting:
-hello = "Hello, "
-greeting = hello + user_name + "!"
+greeting = "Hello, " + user_name + "!"
 print (greeting)
