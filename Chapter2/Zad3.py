2d37aab4aa9114ef83f6ce35c1b4753a2bd65389
Agata Wiewiora <agata.wiewiora@globallogic.com>
2018-03-02 13:25:42 +0100

    corrected Zad3 Chapter2

--- Chapter2/Zad3.py
+++ Chapter2/Zad3.py
@@ -8,11 +8,14 @@ Given a three-digit number. Find the sum of its digits.
 '''
 
 integer = input("Please, provide sample three-digit integer: ")
-if type(integer) == int and integer > 99:
+if type(integer) == int and integer > 99 and integer < 1000 :
     ones_digit = integer % 10
+    print('ones {}'.format(ones_digit))
     hundreds_digit = integer / 100
-    tens_digit = integer / 10 - 10
+    print('hundreds {}'.format(hundreds_digit))
+    tens_digit = (integer / 10) % 10
+    print('tens {}'.format(tens_digit))
     sum_of_digits = ones_digit + tens_digit + hundreds_digit
-    print ('Sum of all digits is {}'.format(sum_of_digits))
+    print('Sum of all digits is {}'.format(sum_of_digits))
 else:
-    print ('Incorrect input value')
+    print('Incorrect input value')
