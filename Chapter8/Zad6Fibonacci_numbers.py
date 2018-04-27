36cd95450025f6ba8d5d7d2e6cd16c0364d10244
Agata Wiewiora <agata.wiewiora@globallogic.com>
2018-04-27 22:11:19 +0200

    zad6 modified

--- Chapter8/Zad6Fibonacci_numbers.py
+++ Chapter8/Zad6Fibonacci_numbers.py
@@ -2,6 +2,29 @@
 
 '''
 
+def fib_recurrent(n):
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        return fib_recurrent(n-2) + fib_recurrent(n-1)
+
+def fib_iter(n):
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        before_last, last = 0, 1
+        for _ in range(2, n+1):
+            last, before_last = last + before_last, last
+            print(last)
+        return last
+
+print(fib_iter(5000))
+for i in range(10):
+    print(i, fib_iter(i))
 
 def fib(n):
     a = []
