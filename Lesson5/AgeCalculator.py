bf985ee96d2ecab31280ba5e24f4940cf956aad5
Agata Wiewiora <agata.wiewiora@globallogic.com>
2018-05-06 20:14:28 +0200

    Lesson5 finished

---
+++ Lesson5/AgeCalculator.py
@@ -0,0 +1,15 @@
+'''
+Age calculator - write a program that for given list of people's
+ages calculate the average age of adults (age >= 18)
+and count the children (age < 18). Use list comprehensions.
+'''
+
+
+def average_age_of_adults(age_list):
+    filter = 18
+    if all(age >= 0 for age in age_list):
+        filtered_list = [age for age in age_list if age >= filter]
+        print reduce(lambda x, y: x + y, filtered_list) / len(filtered_list)
+
+
+average_age_of_adults([2, 18, 30])
