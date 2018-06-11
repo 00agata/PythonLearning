6c575d26ef22a6b31d0cee701dafa69ed19c977c
00agata <agata.wiewiora1@gmail.com>
2018-06-11 22:32:52 +0200

    new zad added

--- Lesson7/ReplaceTheSubstring.py
+++ Lesson7/ReplaceTheSubstring.py
@@ -1,3 +1,20 @@
 '''
 Given a string. Replace in this string all the numbers 1 by the word one.
 '''
+
+sample_string = '111knknjnk111kjkjh11khkh1'
+
+# substring - string to replace
+# sampleString - string to make replacements of substring of this string
+# replacement - new string to put instead of substring
+
+
+def replace_substring(substring, sampleString, replacement):
+    sample_list = list(sample_string)
+    for idx, item in enumerate(sample_list):
+        if item == substring:
+            sample_list[idx] = replacement
+    return str(sample_list)
+
+
+print(replace_substring('1', sample_string, 'one'))
