f26734f6cf5f6278b240739d66d157e4eb19281a
00agata <agata.wiewiora1@gmail.com>
2018-06-26 21:07:14 +0200

    last zad added

--- Lesson8/PhoneNumberValidator.py
+++ Lesson8/PhoneNumberValidator.py
@@ -7,12 +7,13 @@ format with optional country prefix
 
 import re
 
-sample_phone_number = '001999999999'
+sample_phone_number = '0048 123 456 789'
 
 
 def phone_number_validator(sample_phone_number):
     phoneRegex = re.compile(r'''(
-        (\+\d{0,2}|00\d{0,2})?        # are code
+        (\+\d{0,2}|00[0-9]{0,2})?          # are code
+        (\s|-|\.)?                    # separator
         \d{3}                         # first 3 digits
         (\s|-|\.)?                    # separator
         \d{3}                         # second 3 digits
