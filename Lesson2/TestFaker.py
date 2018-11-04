0e425ac677b742be71b8aa7380c524faa9fb73e5
00agata <agata.wiewiora1@gmail.com>
2018-11-04 19:43:43 +0100

    Lesson2 is finished. Let's keep TestFaker class.

--- Lesson2/TestFaker.py
+++ Lesson2/TestFaker.py
@@ -1,4 +1,11 @@
 from faker import Faker
 
 
-faker = Faker()
+myfaker = Faker()
+
+# test names
+
+list_of_fake_names = []
+for i in range (0, 30):
+    list_of_fake_names.append(myfaker.name())
+    print('{}'.format(list_of_fake_names[i]))
