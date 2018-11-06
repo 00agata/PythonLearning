3d3bbd50e60683aa832c13db095156ba271e6922
00agata <agata.wiewiora1@gmail.com>
2018-11-06 23:27:55 +0100

    Lesson3 - collatz sequence done, distance done

---
+++ Lesson3/TheLengthOftheSegment.py
@@ -0,0 +1,17 @@
+'''
+
+'''
+
+import math
+
+
+def distance (x1, y1, x2, y2):
+    return math.sqrt((x2-x1)**2 + (y2-y1)**2)
+
+
+x1 = float(input('x1: '))
+x2 = float(input('x2: '))
+y1 = float(input('y1: '))
+y2 = float(input('y2: '))
+
+print('{0:4.2f}'.format(distance(x1, x2, y1, y2), ))
