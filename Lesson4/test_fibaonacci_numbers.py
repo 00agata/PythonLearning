5c547146751c9dab3c30dc7947711fb475297ca0
00agata <agata.wiewiora1@gmail.com>
2018-11-12 16:51:53 +0100

    testing fib(n) corrected

--- Lesson4/test_fibaonacci_numbers.py
+++ Lesson4/test_fibaonacci_numbers.py
@@ -7,26 +7,12 @@ import pytest
 def fib(n):
     if n is not None:
         if type(n) == int:
-            a = []
-            a1 = 1  # id 1
-            a2 = 1  # id 2
-            a.append(a1)
-            a.append(a2)
-
             if n == 0:
-                return 0  # id 0
-            elif n == 1:
-                return a1  # id 1
-            elif n == 2:
-                return a2  # id 2
-            elif n > 2:
-                print(a[a1])
-                print(a[a2])
-                for i in range(2, n):
-                    an = a[i-2] + a[i-1]
-                    a.append(an)
-                    print(an)
-                return an
+                return 1
+            if n == 1:
+                return 1
+            elif 1 < n <= 1000:
+                return fib(n-1)+fib(n-2)
             elif n > 1000:
                 raise ValueError('Max allowed value is 1000')
             else:
